2c6860edf47a4a669f58a324cee14e457c0563a3
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-05-11 00:10:53 -0400

    add validations

--- app/models/composition.rb
+++ app/models/composition.rb
@@ -1,6 +1,9 @@
 class Composition <ActiveRecord::Base
     belongs_to :list_type
     belongs_to :era
+
+    validates :title, :composer, :instrumentation, presence: true
+
     extend Slugifiable::ClassMethods
     include Slugifiable::InstanceMethods
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User <ActiveRecord::Base
+    has_secure_password
+
+    validates :name, :password_digest, presence: true
     has_many :list_types
     has_many :compositions, through: :list_types
     extend Slugifiable::ClassMethods
