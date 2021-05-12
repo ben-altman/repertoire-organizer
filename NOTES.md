ff860fe3a92f9e38b41796ef082b4ba43a0a44d9
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-05-12 14:59:08 -0400

    build compositions/new

---
+++ NOTES.md
@@ -0,0 +1,11 @@
+<%# <!--<label for="artist_name">Artist Name</label>
+  <input type="text" name="artist[name]" id="artist_name"> </br>
+
+  <label for="name">Name</label>
+  <input type="text" name="song[name]" id="name"> </br>
+
+  <label> Genres </label>
+  <%# #Genre.all.each do |genre| %>
+    <%# <input id="<%= #genre.name %>" type="checkbox" name="genres[]" value="<%= #genre.id %>">
+  <% #end %>
+--> %>
--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -3,4 +3,17 @@ class CompositionsController < ApplicationController
     get '/compositions/new' do
         erb :'/compositions/new'
     end
+
+    post '/compositions/index' do
+
+ binding.pry
+    end
 end
+
+    # @song = Song.create(params[:song])
+    # @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    # @song.genre_ids = params[:genres]
+    # @song.save
+
+    # flash[:message] = "Successfully created song."
+    # redirect("/songs/#{@song.slug}")
