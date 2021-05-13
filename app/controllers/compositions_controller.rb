2c8089576f98d48a1537d77e719d2cb5f3e6d07a
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-05-12 23:55:07 -0400

    fix create compositions

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -1,12 +1,30 @@
 class CompositionsController < ApplicationController
 
+    get '/compositions' do
+binding.pry
+        erb :'compositions/index'    
+    end
+
     get '/compositions/new' do
         erb :'/compositions/new'
     end
 
     post '/compositions/index' do
 
- binding.pry
+        @composition = Composition.create(params[:list][:composition])
+
+        if current_user.list_types.any? {|l| l.name == params[:list][:name]}
+            list = current_user.list_types.where(name: params[:list][:name])
+            @composition.list_type_id = list.first.id
+        else 
+            @list = ListType.create(params[:list][:name])
+            current_user.list_types << @list
+            @composition.list_type_id = @list.id
+        end
+
+        @composition.save
+
+        redirect "/compositions"
     end
 end
 
