a77d63a8e63f4013ca4ef24bc6e6fb775343055a
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-05-13 23:24:06 -0400

    stub remaining composition routes

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -29,28 +29,50 @@ class CompositionsController < ApplicationController
 
     post '/compositions/index' do
 
-        @composition = Composition.create(params[:list][:composition])
-
-        if current_user.list_types.any? {|l| l.name == params[:list][:name]}
-            list = current_user.list_types.where(name: params[:list][:name])
-            @composition.list_type_id = list.first.id
-        else 
-            @list = ListType.create(params[:list][:name])
-            @list.save
-            current_user.list_types << @list
-            @composition.list_type_id = @list.id
+        if logged_in?
+            @composition = Composition.create(params[:list][:composition])
+            if current_user.list_types.any? {|l| l.name == params[:list][:name]}
+                list = current_user.list_types.where(name: params[:list][:name])
+                @composition.list_type_id = list.first.id
+            else 
+                @list = ListType.create(params[:list][:name])
+                @list.save
+                current_user.list_types << @list
+                @composition.list_type_id = @list.id
+            end
+            @composition.save
+            redirect "/compositions"
+        else
+            redirect '/'
         end
-
-        @composition.save
-
-        redirect "/compositions"
     end
 
     get '/compositions/:slug/edit' do
-        @composition = Composition.find_by_slug(params[:slug])
-        erb :'compositions/edit'
+        if logged_in?
+            @composition = Composition.find_by_slug(params[:slug])
+            erb :'compositions/edit'
+        else
+            redirect '/'
+        end
     end
 
+    patch '/compositions/:slug' do
+        if logged_in?
+
+
+        else
+            redirect '/'
+        end
+    end
+
+    delete '/compositions/:slug/delete' do
+        if logged_in?
+
+
+        else
+            redirect '/'
+        end
+    end
 
 end
 
